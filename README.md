b3f46e676e9fca40d3503c1678ce48d7e01b695d
PJ Lindsay <pjlindsay@gmail.com>
2020-10-09 11:45:46 -0600

    Add scoped slot example

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ take deeper look at Components in Vue 3
 - a better way to register components (Global vs Local)
 - scoped style: keep styling for the local template only (non-global style)
 - slots: allow us to receive HTML content (incl Vue features) from outside component
+- scoped slots: allow some aspects of component to be customizable
--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,13 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></UserInfo>
+    <!-- scoped slot: allows us to STYLE content
+      don't need template wrapper if all markup can go into one slot
+    -->
+    <course-goals #default="slotProps">
+      <h2> {{ slotProps.item }}</h2>
+      <p>{{ slotProps['another-prop'] }}</p>
+    </course-goals>
   </div>
 </template>
 
@@ -16,6 +23,7 @@
 import TheHeader from './components/TheHeader.vue'
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
 
@@ -24,7 +32,8 @@ export default {
   components: {
     BadgeList,
     TheHeader,
-    UserInfo
+    UserInfo,
+    CourseGoals
   },
 
   data() {
